c780565db053ca528ba55bc846c7ad3bcd67bb7c
Azeb Tesfay <azbseb@gmail.com>
2025-10-02 14:52:09 -0700

    Dog class

---
+++ src/dog.java
@@ -0,0 +1,30 @@
+
+class Dog implements Animal {
+
+	/**
+	 * @return
+	 *         A string
+	 *         containing the
+	 *         ASCII art
+	 *         of the dog.
+	 */
+
+	@Override
+	public String getAnimalArt() {
+		return "        \\   .\n" +
+				"         \\  ..^____/\n" +
+				"            `-. ___ )\n" +
+				"              ||   ||\n";
+	}
+
+	/**
+	 * Returns the name of the animal ("duck").
+	 * This method overrides the toString method to return the name of the duck.
+	 *
+	 * @return The string "duck" representing the name of the animal.
+	 */
+	@Override
+	public String toString() {
+		return "duck";
+	}
+}
