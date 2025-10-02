39f06fed2d26fa5871b17a26b49a5bbf71480602
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-10-02 14:47:06 -0700

    Added deer to sayapp

--- src/SayApp.java
+++ src/SayApp.java
@@ -71,12 +71,12 @@ public class SayApp {
 
     /**
      * Provides a list of available animals.
-     * Currently, the only available animals are Duck and Cow
+     * Currently, the only available animals are Duck, Cow and Deer
      *
      * @return A list of Animal objects.
      */
     public static List<Animal> animalList() {
-      return Arrays.asList(new Cow(), new Duck());
+      return Arrays.asList(new Cow(), new Duck(), new Deer());
     }
 
     /**
